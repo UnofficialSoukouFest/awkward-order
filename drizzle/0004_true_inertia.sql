8d5ed08d1c1d5a432fa7fda063af877c60d2f3bd
kanium3 <dev@kanium.me>
2025-08-29 19:44:24 +0900

    drizzle-kit pushが動くように
    何故か動く

--- drizzle/0004_true_inertia.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `product` RENAME COLUMN "root_ingredients" TO "rootIngredients";--> statement-breakpoint
-ALTER TABLE `product` RENAME COLUMN "composite_ingredients" TO "compositeIngredients";
