86fd0b600ed3a2fa4cb574e3d1ee98bf4a528389
ameirok <151885371+proirok@users.noreply.github.com>
2025-08-31 09:45:09 +0000

    メニュー一覧用のカードの作成完了(通常版)

---
+++ app/component/card/select-card.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import styles from "./select-card.module.css"
+
+export function SelectCard(){
+    const [ isOpen, setIsOpen ] = useState(false)
+    return(
+        <div className={styles.card}>
+            <div className={styles.main}>
+                <img alt="商品の画像です" />
+                <div className={styles.menuPrice}>
+                    <p className={styles.menu}>商品名＊＊＊＊</p>
+                    <p className={styles.price}>¥100</p>
+                </div>
+            </div>
+            {isOpen ? (
+                <>
+                    <button onClick={() => setIsOpen(false)}>表示を折りたたむ</button>
+                    <div className={styles.ingredients}>
+                        <div className={styles.allergen}>
+                            <h3>特定原材料28品目</h3>
+                            <p>特定原材料＊＊＊＊＊＊</p>
+                        </div>
+                        <div className={styles.otherIngredients}>
+                            <h3>原材料</h3>
+                            <p>原材料＊＊＊＊＊＊＊＊</p>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                <button onClick={() => setIsOpen(true)}>原材料・アレルゲンを表示</button>
+                </>
+            )}
+        </div>
+    )
+}
